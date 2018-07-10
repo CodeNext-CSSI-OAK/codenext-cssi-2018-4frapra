865fda631d3621e151d7987eff8274e36fe43f3a
Student <student@Admins-MacBook-Air.local>
2018-07-10 12:38:11 -0700

    update

--- hello.js
+++ hello.js
@@ -1,5 +1,14 @@
 // Author: Francisco Prado
 
-console.log("Hello World!");
-console.log("Welcome Coaches of CSSI!!");
-console.log("We are ready to learn.");
+let readline = require("readline-sync");
+
+let age = Number(readline.question("What is your age? "))
+
+//let ageAsAString = readline.question("What is your age? ")
+//let age = number(ageAsAString);
+
+console.log("In 2028 you will be " + (age + 10) + " years old.");
+
+//console.log("Hello World!");
+//console.log("Welcome Coaches of CSSI!!");
+//console.log("We are ready to learn.");
